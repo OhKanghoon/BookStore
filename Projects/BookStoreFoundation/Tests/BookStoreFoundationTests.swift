52de9b0f1f32ad0bd52431a46d669e29a1bc01ed
OhKanghoon <ggaa96@naver.com>
2020-12-30 16:20:35 +0900

    BookStoreFoundation 타겟 추가

---
+++ Projects/BookStoreFoundation/Tests/BookStoreFoundationTests.swift
@@ -0,0 +1,8 @@
+@testable import BookStoreFoundation
+
+final class BookStoreFoundationTests: XCTestCase {
+  func testExample() throws {
+      // This is an example of a functional test case.
+      // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+}
